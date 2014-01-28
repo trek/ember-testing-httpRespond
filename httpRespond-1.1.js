ca4b4b41b3ae6dc78c8a082a25487128fe3036af
jamienglish <jamienglish@gmail.com>
2014-01-28 10:16:18 +0000

    Throw an Ember.Error rather than a string to ensure tests fail when thrown

--- httpRespond-1.1.js
+++ httpRespond-1.1.js
@@ -8,7 +8,7 @@ Ember.Test.registerHelper('httpRespond', function(app, verb, url, body, status) 
       found.respond(status || 200, {'content-type': 'application/json'}, body);
     });
   } else {
-    throw("No request intercepted for " + verb.toUpperCase() + " " + url + ". Intercepted requests were: " + fakehr.requests.map(function(r){ return r.method + " " + r.url}).join(", "));
+    throw new Ember.Error("No request intercepted for " + verb.toUpperCase() + " " + url + ". Intercepted requests were: " + fakehr.requests.map(function(r){ return r.method + " " + r.url}).join(", "));
   }
   return wait(app);
 });
