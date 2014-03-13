fa57b55456544462022b7de19902cfc0fc776ee2
Robert Jackson <robert.w.jackson@me.com>
2014-03-12 22:02:12 -0400

    Add new helper visitAndRespond to fix issues with Ember >= 1.4.0.
    Details from #10:
    
    1. The `wait` helper in `ember-testing` is intended to wait for the
    routing transition to be completed (See
    [here](https://github.com/emberjs/ember.js/blob/master/packages_es6/ember-testing/lib/helpers.js#L146-148)).
    2. `httpRespond` is setup via `registerAsyncHelper`.
    3. An async helper from the perspective of the `ember-testing` package
    chains off of the previous promise.
    4. Because of the above, `httpRespond` never gets a chance to respond,
    and causes the test to hang (waiting on the router to resolve its
    promise which will never happen because httpRespond was responsible for
    resolving it).
    
    The reason this worked up until
    https://github.com/emberjs/ember.js/pull/4347, was that the router was
    completely refactored (prior to 1.0.0-rc.6) and the flag that
    `ember-testing` was previously checking `router.isLoading` was NEVER set
    so it was always false (and therefore would never wait for the routers
    transition to resolve).  The change made in that PR was a good change,
    and brought the `wait` helper back inline with what I assume was its
    intent all along.
    
    ---------
    
    OK, all that said, I have a solution, but it isn't API compatible with
    the prior versions.  Basically, we need to have the `visit` AND the
    `httpRespond` bundled together in a single async helper.  This allows us
    to kick off the router transition, and then handle the canned response
    before calling `wait` (and only `wait`'ing at the end of both).
    
    This may or may not be an acceptable solution to @trek, but it should
    work and resolve the issue reported here.

--- httpRespond-1.1.js
+++ httpRespond-1.1.js
@@ -1,4 +1,4 @@
-Ember.Test.registerAsyncHelper('httpRespond', function(app, verb, url, body, status) {
+function handleHttpRespond(app, verb, url, body, status) {
   if(typeof body !== 'string'){ body = JSON.stringify(body); }
 
   var found = fakehr.match(verb.toUpperCase(), url)
@@ -11,14 +11,31 @@ Ember.Test.registerAsyncHelper('httpRespond', function(app, verb, url, body, sta
     throw new Ember.Error("No request intercepted for " + verb.toUpperCase() + " " + url + ". Intercepted requests were: " + fakehr.requests.map(function(r){ return r.method + " " + r.url}).join(", "));
   }
   return wait(app);
+}
+
+Ember.Test.registerAsyncHelper('visitAndRespond', function(app, path, verb, url, body, status) {
+  var router = app.__container__.lookup('router:main');
+  router.location.setURL(path);
+
+  if (app._readinessDeferrals > 0) {
+    router['initialURL'] = path;
+    Ember.run(app, 'advanceReadiness');
+    delete router['initialURL'];
+  } else {
+    Ember.run(app, app.handleURL, path);
+  }
+
+  handleHttpRespond(app, verb, url, body, status);
 });
 
+Ember.Test.registerAsyncHelper('httpRespond', handleHttpRespond);
+
 /*
-  This is mildly crazy. Ember.Test increments requets on ajax start
-  so promises won't resolve until all xhrs complete, but were mocking
-  them all, so we just remove the ajaxStart and ajaxStop callbacks
-  until we can merge into master and put some of this behind flags.
-*/
+ This is mildly crazy. Ember.Test increments requets on ajax start
+ so promises won't resolve until all xhrs complete, but were mocking
+ them all, so we just remove the ajaxStart and ajaxStop callbacks
+ until we can merge into master and put some of this behind flags.
+ */
 Ember.Test.onInjectHelpers(function() {
   Ember.$(document).unbind("ajaxStart ajaxStop ajaxSend ajaxComplete");
 });
